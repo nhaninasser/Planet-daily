608eab5dacc812d1f814275fd1495d69483ddb1d
Nasser Nasser <nhani.nasser@gmail.com>
2022-04-15 23:35:50 -0400

    added current date and time using moment formatting

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,10 @@
 const d = new Date();
 console.log(d);
 
+now = moment().format('LLLL');
+
+const currentDate = $('#currentDay');
+    currentDate.text(now);
 
 var currentHour = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
 for (var i = 0; i < currentHour.length; i++) {
