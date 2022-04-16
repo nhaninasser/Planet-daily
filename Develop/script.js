99f1234f5a27ae9c40c95f7f9c96d58290c666d7
Nasser Nasser <nhani.nasser@gmail.com>
2022-04-15 23:52:20 -0400

    set all rows text retrieval

--- Develop/script.js
+++ Develop/script.js
@@ -32,6 +32,16 @@ $(".saveBtn").on("click", function () {
 
 function getText() {
     $("#8 .description").val(localStorage.getItem("8"));
+    $("#9 .description").val(localStorage.getItem("9"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#13 .description").val(localStorage.getItem("13"));
+    $("#14 .description").val(localStorage.getItem("14"));
+    $("#15 .description").val(localStorage.getItem("15"));
+    $("#16 .description").val(localStorage.getItem("16"));
+    $("#17 .description").val(localStorage.getItem("17"));
+    $("#18 .description").val(localStorage.getItem("18"));
 }
 
 getText();
