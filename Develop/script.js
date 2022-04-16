d70056bf8aa35b5acafca06633bf379d807b5ea3
Nasser Nasser <nhani.nasser@gmail.com>
2022-04-15 23:41:12 -0400

    aded save functionality to local storage

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,13 @@
 const d = new Date();
 console.log(d);
 
+//Current date and time formatting using moment
 now = moment().format('LLLL');
 
 const currentDate = $('#currentDay');
     currentDate.text(now);
 
+// adding color change to rows   
 var currentHour = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
 for (var i = 0; i < currentHour.length; i++) {
     if (d < currentHour[i]) {
@@ -21,3 +23,9 @@ for (var i = 0; i < currentHour.length; i++) {
             .addClass("present");
     }
 }
+//Save functionality to local storage 
+$(".saveBtn").on("click", function () {
+    var time = $(this).parent().attr("id");
+    var textContent = $(this).siblings(".description").val();
+    localStorage.setItem(time, textContent);
+});
